642c8fe43ef0b5b3e7917ee12478173c5f23e611
YoichiroTacker <youitiro.tacker@gmail.com>
2024-02-08 12:47:18 +0900

    prepare add ssn

---
+++ build/_deps/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/build/_deps/src/googletest
+# Build directory: /home/build/_deps/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
